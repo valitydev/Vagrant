a016d5a2a24019c902fc57d8b9164e8065e27b7d
joker2013 <ivan.joker2009@mail.ru>
2020-11-27 22:14:24 +0300

    first commit

---
+++ ubuntu_20.04/bootstrap.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
+  && chmod +x minikube
+mkdir -p /usr/local/bin/
+install minikube /usr/local/bin/
+apt update
+apt install virtualbox
