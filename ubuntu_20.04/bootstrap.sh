82e07137b31576d4324da631075671f6b98a57c0
joker2013 <ivan.oker2009@mail.ru>
2021-03-12 17:16:51 +0300

    Update

--- ubuntu_20.04/bootstrap.sh
+++ ubuntu_20.04/bootstrap.sh
@@ -1,8 +1,1 @@
 #!/usr/bin/env bash
-
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
-  && chmod +x minikube
-mkdir -p /usr/local/bin/
-install minikube /usr/local/bin/
-apt update
-apt install virtualbox
